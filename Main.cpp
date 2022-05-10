7eb65f31935d825ddddf977fbbd2230fdf8ca43d
hyunjaesong <thflro1100@naver.com>
2022-05-10 14:43:03 +0900

    TWO

--- Main.cpp
+++ Main.cpp
@@ -1,36 +1,65 @@
-#include <iostream>
-using namespace std;
+//#include <iostream>
+//using namespace std;
+//
+//int Plus(int* Array, int count)
+//{
+//	int sum = 0;
+//	for (int i = 0; i < count; ++i)
+//	{
+//		sum += Array[i];
+//	}
+//	return sum;
+//}
+//
+//void Sort(int* Array, int Count)
+//{
+//	for (int i = 0; i < Count; ++i)
+//	{
+//		for (int j = i; j < Count; ++j)
+//		{
+//			if (Array[i] < Array[j])
+//				{
+//						int Temp = Array[i];
+//						Array[i] = Array[j];
+//						Array[j] = Temp;
+//					
+//				}
+//
+//		}
+//	}
+//}
+//
+//
+//
+//
+//int main()
+//{
+//	int Count = 0;
+//
+//	cin >> Count;
+//
+//	int* Array = new int[Count];
+//
+//	for (int i = 0; i < Count; ++i)
+//	{
+//		cin >> Array[i];
+//
+//	}
+//
+//
+//	int sum = Plus(Array, Count);
+//	cout << sum << endl;
+//
+//	Sort(Array, Count);
+//	
+//	for (int i = 0; i < Count; i++)
+//	{
+//		cout << Array[i] << endl;
+//	}
+//	
+//	return 0;
+//
+//}
+//	
+//	
 
-
-int Plus(int* Array, int size)
-{
-	int sum = 0;
-
-	for (int i = 0; i < size; i++)
-	{
-		sum += Array[i];
-	}
-
-	return sum;
-
-}
-int main()
-{ 
-
-	int size = 0;
-
-	cin >> size;
-
-	int* Array = new int[size];
-
-	for (int i = 0; i < size; i++)
-	{
-		cin >> Array[i];
-	
-	}
-
-	cout << Plus(Array, size) << endl;
-		
-	return 0;
-
-}
